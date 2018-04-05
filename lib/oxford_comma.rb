ed6d4b331377d42dad20697c558cda5c2a8fb3c8
Mary Jack <maryjackinthebox@gmail.com>
2018-04-05 02:49:00 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
 def oxford_comma(array)
+ 
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.insert(-2, 'and')
+    array.join(" ")
+  elsif array.length == 3
+    array.insert(-2, 'and')
+    str = array.join(', ')
+    newArr = str.split
+    newArr[-2] = 'and'
+    newArr.join(" ")
+  else
+    array.length > 4
+    array[-1] = "and #{array[-1]}"
+    array.join(', ')
+end
 
 end
